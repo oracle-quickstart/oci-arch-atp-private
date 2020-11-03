61b742ddbbd47c5d7741a70257eb599c999ad8b3
lfeldman <lukasz.feldman@oracle.com>
2020-11-03 13:06:15 +0100

    Updated ORM stack2

--- README.md
+++ README.md
@@ -55,8 +55,8 @@ fingerprint          = "<finger_print>"
 private_key_path     = "<pem_private_key_path>"
 
 # SSH Keys
-ssh_public_key  = "<public_ssh_key_path>"
-ssh_private_key  = "<private_ssh_key_path>"
+ssh_public_key  = "<public_ssh_key>"
+ssh_private_key  = "<private_ssh_key>"
 
 # Region
 region = "<oci_region>"
--- webserver1.tf
+++ webserver1.tf
@@ -24,7 +24,7 @@ resource "oci_core_instance" "Webserver1" {
     source_id   = lookup(data.oci_core_images.OSImageLocal.images[0], "id")
   }
   metadata = {
-    ssh_authorized_keys = file(var.ssh_public_key)
+    ssh_authorized_keys = var.ssh_public_key != "" ? var.ssh_public_key : tls_private_key.public_private_key_pair.public_key_openssh
   }
   create_vnic_details {
     subnet_id = oci_core_subnet.WebSubnet.id
