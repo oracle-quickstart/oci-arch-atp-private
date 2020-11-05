2448d47e64c8dde0a717e8d4301884e359dba907
lfeldman <lukasz.feldman@oracle.com>
2020-11-05 13:42:20 +0100

    Fixes to ORM support

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "ssh_private_key" {
-  value = var.ssh_private_key != "" ? var.ssh_private_key : tls_private_key.public_private_key_pair.private_key_pem
+output "generated_ssh_private_key" {
+  value = tls_private_key.public_private_key_pair.private_key_pem
+}
+
+output "webserver1_public_ip" {
+  value = [data.oci_core_vnic.Webserver1_VNIC1.public_ip_address]
 }
