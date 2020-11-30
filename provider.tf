e44e481028ea0324737b3985ef33b2d08d974e17
lfeldman <lukasz.feldman@oracle.com>
2020-11-30 18:12:38 +0100

    Updates to schema.yaml

--- provider.tf
+++ provider.tf
@@ -4,9 +4,9 @@
 provider "oci" {
   version          = ">= 3.64.0" 
   tenancy_ocid     = var.tenancy_ocid
+  region           = var.region
   fingerprint      = var.fingerprint
   user_ocid        = var.user_ocid
-  region           = var.region
   private_key_path = var.private_key_path
 }
 
